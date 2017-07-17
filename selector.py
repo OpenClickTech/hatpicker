1e2ba1f4b1aedf76c07d4ae5f1951f88054e0440
Colin Matthias <cmatthias@gmail.com>
2017-07-17 11:57:52 -0700

    Initial commit

---
+++ selector.py
@@ -0,0 +1,21 @@
+from random import SystemRandom
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Draw N ids at random from an input file.')
+parser.add_argument('-n', type=int, help='the number of ids to draw')
+args = parser.parse_args()
+
+rng = SystemRandom()
+ids = set()
+
+for line in sys.stdin:
+    ids.add(line.strip())
+
+chosen = rng.sample(ids, args.n)
+
+print('Drawing {} ids from a set of {}'.format(args.n, len(ids)))
+
+for c in chosen:
+    print('got {}'.format(c))
+
