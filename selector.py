9af57b31a470bc7aacd7d527a9c2fa141829940a
Colin Matthias <cmatthias@gmail.com>
2017-07-18 07:26:36 -0700

    Clean up and comment selector.py

--- selector.py
+++ selector.py
@@ -6,16 +6,22 @@ parser = argparse.ArgumentParser(description='Draw N ids at random from an input
 parser.add_argument('-n', type=int, help='the number of ids to draw', required=True)
 args = parser.parse_args()
 
-rng = SystemRandom()
 ids = set()
 
+# Collect all the ids, from standard input, ignoring duplicates that appear more
+# than once in the input file.
 for line in sys.stdin:
     ids.add(line.strip())
 
-chosen = rng.sample(ids, args.n)
-
 print('Drawing {} ids from a set of {}'.format(args.n, len(ids)))
 
-for c in chosen:
-    print('got {}'.format(c))
+# Initialize random number generator
+rng = SystemRandom()
+
+# Choose n values at random from the list
+chosen = rng.sample(ids, args.n)
+
+# Print the choices out in order
+for idx, c in enumerate(chosen):
+    print('Pick {}: {}'.format(idx+1, c))
 
